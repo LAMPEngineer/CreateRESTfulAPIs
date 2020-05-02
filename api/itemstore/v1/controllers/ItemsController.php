ad724424ae57f82a7d9378ba1ae5e28324bf8093
LAMPEngineer <LAMP.Engineer@yahoo.com>
2020-05-02 18:35:47 +0530

    added version

---
+++ api/itemstore/v1/controllers/ItemsController.php
@@ -0,0 +1,87 @@
+<?php
+	
+/*
+ *  Items controller to have actions for items
+ */
+use RequestController as Request;
+
+class ItemsController
+{
+	/**
+	 * to hold db connection object
+	 * @var object
+	 */
+	private $conn;
+
+	/**
+	 * construct initialize db connection object
+	 */
+	public function __construct()
+	{
+		$db = new DatabaseConfig;
+		$this->conn = $db->connect();	
+	}
+
+	/**
+	 * Action for GET verb
+	 * for both individual resource 
+	 * as well as a collection
+	 * 
+	 * @param  Request $request object
+	 * @return array            dara
+	 */
+	public function getAction(Request $request):array
+	{
+
+		// get item id from request url
+		if(isset($request->url_elements[5])){
+
+			$item_id = (int)$request->url_elements[5];
+
+			// invalid item id
+			if(($item_id == 0) or empty($item_id)){
+
+				$data['message'] = 'invalid item id.';
+				$data['status'] = '0';
+
+				return $data;
+
+			} else{
+				//get a resource
+				$data['message'] = 'here is the info of item '. $item_id;
+			}			
+		} else{
+			// get collection
+			$data['message'] = 'you want a list of items';
+			$item_id = '0';
+		}
+
+		//Get item model object
+		$items = new ItemModel($this->conn);
+		$result = $items->read($item_id);
+
+		// Get row count
+  		$num = $result->rowCount();
+  		if($num > 0){
+
+  			//fetch PDO result set
+  			$data['data'] = $result->fetchAll(PDO::FETCH_ASSOC);
+  			$data['status'] = '1';
+
+  		} else {
+
+  			$data['message'] = 'no data found';
+  			$data['status'] = '0';
+  		}		
+
+		return $data;
+	}
+
+	public function postAction(Request $request)
+	{
+		$data = $request->parameters;
+		$data['message'] = 'This data was submitted';
+		return $data;
+	}	 
+
+}
---
+++ api/itemstore/v1/index.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ *  Request handler that route request to right place.
+ *  It calls action of the controller and return back
+ *  response in right format as requested.
+ *  
+ */
+
+// check PATH_INFO
+if(!isset($_SERVER['PATH_INFO'])){
+	// redirect to root
+	header("Location: ../../../");
+}
+
+//autoload 
+include __DIR__ . '\autoload.php';
+
+// request object
+$request = new RequestController();
+
+// route the request to the right place
+$controller_name = ucfirst($request->url_elements[4]) . 'Controller';
+
+
+if(class_exists($controller_name)){
+	
+	$controller = new $controller_name();
+	// call action
+	$action_name = strtolower($request->verb) . 'Action';
+	
+	$result = $controller->$action_name($request);
+	// view format
+	$view_name = ucfirst($request->format) . 'View';
+
+	if(class_exists($view_name)){
+
+		$view = new $view_name();
+		$view->render($result);		
+	}
+
+
+}
+
+
